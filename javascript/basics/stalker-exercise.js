ae5ab45317fb5b38b2defa3b6f84a80ebd3154de
Vinicius Bernardo <25652392+vinibern5@users.noreply.github.com>
2018-09-24 19:53:18 -0300

    add some basic js exercises

---
+++ javascript/basics/stalker-exercise.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Your first name");
+var lastName = prompt("Your last name");
+var age = prompt("Your age");
+
+console.log("Your full name is ".concat(firstName).concat(" ").concat(lastName));
+console.log("Your age is " + age);
