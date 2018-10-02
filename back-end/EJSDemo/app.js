c5c7e857ce69197b699a1629df10f96605642ce9
Vinicius Bernardo <25652392+vinibern5@users.noreply.github.com>
2018-10-01 21:47:09 -0300

    add views with if and for loops

--- back-end/EJSDemo/app.js
+++ back-end/EJSDemo/app.js
@@ -10,4 +10,20 @@ app.get("/love/:name", function(request, response) {
     response.render("love.ejs", {name: name});
 });
 
+app.get("/foods/:favoriteFood", function(request, response) {
+    var favoriteFood = request.params.favoriteFood;
+    response.render("conditional.ejs", {favoriteFood: favoriteFood});
+});
+
+app.get("/posts/", function(request, response) {
+    var posts = [
+        {title: "First", text: "Some Text"}, 
+        {title: "Second", text: "Another Text"}, 
+        {title: "Third", text: "The last text"}
+    ];
+    
+    response.render("posts.ejs", {posts: posts});
+});
+
+
 app.listen(3000);
